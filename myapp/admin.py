616659594204cb7f974de00e2b0b19a02456b8e3
chandra mouli <pchandramouli92@gmail.com>
2020-09-04 23:03:35 +0530

    2nd commit

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from myapp import models
 
 # Register your models here.
+class TopicAdminView(admin.ModelAdmin):
+    list_display=('Topic_name')
+    search_fields=('Topic_name')
+class WebpageAdminView(admin.ModelAdmin):
+    list_display=('Topic','name','url')
+    search_field=('Topic','name')
+    list_editable=('name')
+    list_filter=('Topic')
+class AccessDetailsAdminView(admin.ModelAdmin):
+    list_display=('Webpage','datetime')
+    search_fields=('Webpage','datetime')
+admin.site.register(models.Topic)
+admin.site.register(models.Webpage)
+admin.site.register(models.AccessDetails)
--- myapp/models.py
+++ myapp/models.py
@@ -2,16 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class topic(models.Model):
+class Topic(models.Model):
     topic_name=models.CharField(max_length=100,unique=True,blank=False)
+    def _str_(self):
+        return self.topic_name
 
-class webpage(models.Model):
-    topic=models.ForeignKey(topic,on_delete=models.CASCADE)
+class Webpage(models.Model):
+    topic=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100,unique=True,blank=False)
     url=models.URLField(max_length=100,unique=True,blank=False)
+    def  _str_(self):
+        return self.name
+        
 
-class accessDetails(models.Model):
-    webpage=models.ForeignKey(webpage,on_delete=models.CASCADE)
+class AccessDetails(models.Model):
+    webpage=models.ForeignKey(Webpage,on_delete=models.CASCADE)
     datetime=models.DateTimeField(auto_now_add=True)
+    def _str_(self):
+        return str(self.datetime).split(" ")[0]
 class am(models.Model):
     datetime=models.DateTimeField(auto_now_add=True)
